111101be4334daef1dd00c696532a58d3e7f59a8
Elias Godson <eng.eliasgodson@gmail.com>
2024-06-22 17:22:21 +0300

    Replace sigmoid function definition with stabilized version

--- neural_network/model.py
+++ neural_network/model.py
@@ -2,7 +2,7 @@ import numpy as np
 
 #sigmoid function and its derivative
 def sigmoid(x):
-    return 1 / (1+np.exp(-x))
+   return 1 / (1 + np.exp(-np.clip(x, -500, 500)))
 
 def sigmoid_derivative(x):
     return x * (1-x)
